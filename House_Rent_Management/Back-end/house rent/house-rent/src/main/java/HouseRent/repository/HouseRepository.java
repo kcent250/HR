5e29075d7f6b20e9b4801746f5d07783d5edbb6c
kcent250 <real.innocent250@gmail.com>
2025-05-24 17:32:02 +0300

    Add files via upload

---
+++ House_Rent_Management/Back-end/house rent/house-rent/src/main/java/HouseRent/repository/HouseRepository.java
@@ -0,0 +1,8 @@
+package HouseRent.repository;
+
+
+import com.house.rent.house.rent.model.House;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HouseRepository extends JpaRepository<House, Long> {
+}
---
+++ House_Rent_Management/Back-end/house rent/house-rent/src/main/java/HouseRent/repository/RentalAgreementRepository.java
@@ -0,0 +1,10 @@
+package HouseRent.repository;
+
+import com.house.rent.house.rent.model.RentalAgreement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface RentalAgreementRepository extends JpaRepository<RentalAgreement, Long> {
+    List<RentalAgreement> findByTenantId(Long tenantId);
+    List<RentalAgreement> findByHouseId(Long houseId);
+}
